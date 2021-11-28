c33b1d936f5c5ed8fe52af8dd003db3957e54fec
youxuan12138 <92509541+youxuan12138@users.noreply.github.com>
2021-11-28 17:41:55 +0800

    暂时框架

---
+++ springboot/src/main/java/com/example/demo/common/MybatisPlusConfig.java
@@ -0,0 +1,48 @@
+package com.example.demo.common;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.OptimisticLockerInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+/**
+ * MybatisPlus配置类
+ *
+ */
+//事务处理
+@EnableTransactionManagement
+@Configuration
+@MapperScan("com.example.demo.mapper")
+public class MybatisPlusConfig {
+
+    /**
+     * 分页插件
+     */
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
+    }
+    @Bean
+    public PaginationInterceptor paginationInterceptor() {
+        PaginationInterceptor paginationInterceptor = new PaginationInterceptor();
+        // 你的最大单页限制数量，默认 500 条，小于 0 如 -1 表示不受限制
+        // paginationInterceptor.setLimit();
+        return paginationInterceptor;
+    }
+
+    /**
+     * 乐观锁配置
+     * @return
+     */
+    @Bean
+    public OptimisticLockerInterceptor optimisticLockerInterceptor() {
+        return new OptimisticLockerInterceptor();
+    }
+}
---
+++ vue/src/components/Aside.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+
+    <!--calc后100vh - 50 px空格不能少-->
+    <el-menu
+
+        style="width: 200px;min-height:calc(100vh - 50px)"
+        default-active="2"
+        class="el-menu-vertical-demo"
+    >
+      <el-sub-menu index="1">
+        <template #title>
+          <el-icon><location /></el-icon>
+          <span>选项一</span>
+        </template>
+        <el-menu-item-group title="Group One">
+          <el-menu-item index="1-1">item one</el-menu-item>
+          <el-menu-item index="1-2">item one</el-menu-item>
+        </el-menu-item-group>
+
+      </el-sub-menu>
+      <el-menu-item index="2">
+        <el-icon><icon-menu /></el-icon>
+        <span>Navigator Two</span>
+      </el-menu-item>
+      <el-menu-item index="3" disabled>
+        <el-icon><document /></el-icon>
+        <span>Navigator Three</span>
+      </el-menu-item>
+      <el-menu-item index="4">
+        <el-icon><setting /></el-icon>
+        <span>Navigator Four</span>
+      </el-menu-item>
+    </el-menu>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Aside"
+}
+</script>
+
+<style scoped>
+
+</style>
